1d61e7f797290ecd76cab365dd73f791a77bf9e4
Alex Oficina <github@mycodes4you.com>
2020-11-19 18:00:36 -0600

    ultima de hoy oficina

---
+++ notas-alex.php
@@ -0,0 +1,40 @@
+<?php
+
+$apache_off = 'sudo systemctl stop apache2';
+$apache_on = 'sudo systemctl start apache2';
+$apache_restart = 'sudo systemctl restart apache2';
+$apache_reload = 'sudo systemctl reload apach';
+$apache_start_off = 'sudo systemctl disable apache2';
+$apache_start_on = 'sudo systemctl enable apache2';
+
+
+$mysql_status = 'systemctl status mysql.service';
+
+
+controldb
+AlexRami07
+Jdw7z.eW2z
+
+
+phpmyadmin
+autoshop
+6tb1f1Oc
+
+
+
+clave ssh
+autoshop
+Pf33.AxMDup.t8xm
+
+Myx9ln.2Rhz4rvp-1
+
+701
+Rjf6ge.Fa
+
+
+
+admin_kumo
+Pv0@zr04
+
+
+?>
